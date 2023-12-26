fb14ef76e32cccc453feb04e001b8cddb7adf852
Matt <matthewjosefcruz@gmail.com>
2023-12-27 07:33:59 +0800

    Add Table Generation Of Standard Deviation Calculations

--- script.js
+++ script.js
@@ -3,17 +3,22 @@ const textInput = document.querySelector("form > input");
 const error = document.querySelector("p.error")
 const tableSpace = document.querySelector("div.output");
 
+const tableHead = document.querySelector("thead");
+const tableBody = document.querySelector("tbody");
+const tableFoot = document.querySelector("tfoot");
+
 const table = (() => {
     let arraySet = [], input;
     form.addEventListener("submit", (e) => {
+        resetTable();
         e.preventDefault;
-        console.log("User: " + textInput.value);//
         convertArray(textInput.value);
         if(!isValid()) return;
         updateText();
         console.log("It is valid!");//
+        generateTable();
     });
-    
+
     const convertArray = ((x) => {
         arraySet = x.split(",");
         for(let i = 0; i < arraySet.length; i++) { 
@@ -39,6 +44,92 @@ const table = (() => {
         textInput.value = formattedInput;
     });
     const generateTable = (() => {
-
+        let row, head, cell;
+        const findMean = (() => {
+            let total = 0;
+            for(let x of arraySet) {
+                total += +x;
+            }
+            return total/arraySet.length;
+        });
+        const findStandardDeviation = ((number) => {
+            let current = 0;
+            for(let i = 0; i < arraySet.length; i++) {
+                current += (arraySet[i] - mean) ** 2;
+            }
+            return Math.sqrt(current/number);
+        });
+        const mean = findMean();
+        //DOM Elements
+        const heading = (() => {
+            row = document.createElement("tr");
+            tableHead.appendChild(row);
+            head = document.createElement("th");
+            head.textContent = "value (x)";
+            row.appendChild(head);
+            head = document.createElement("th");
+            head.textContent = "mean (X̄)";
+            row.appendChild(head);
+            head = document.createElement("th");
+            head.textContent = "(x - X̄)";
+            row.appendChild(head);
+            head = document.createElement("th");
+            head.textContent = "(x - X̄)";
+            row.appendChild(head);
+            cell = document.createElement("sup");
+            cell.textContent = 2;
+            head.appendChild(cell);
+        })();
+        const body = (() => {
+            for(let i = 0; i < arraySet.length; i++) {
+                row = document.createElement("tr");
+                tableBody.appendChild(row);
+                cell = document.createElement("td");
+                cell.textContent = parseFloat((arraySet[i] * 1).toFixed(4));
+                row.appendChild(cell);
+                cell = document.createElement("td");
+                cell.textContent = parseFloat(mean.toFixed(4));
+                row.appendChild(cell);
+                cell = document.createElement("td");
+                cell.textContent = parseFloat((arraySet[i] - mean).toFixed(4));
+                row.appendChild(cell);
+                cell = document.createElement("td");
+                cell.textContent = parseFloat(((arraySet[i]-mean) ** 2).toFixed(4));
+                row.appendChild(cell);
+            }
+        })();
+        const footer = (() => {
+            row = document.createElement("tr");
+            tableFoot.appendChild(row);
+            head = document.createElement("th");
+            head.colSpan = 2;
+            head.textContent = "Sample Standard Deviation (s)";
+            row.appendChild(head);
+            head = document.createElement("th");
+            head.colSpan = 2;
+            head.textContent = parseFloat(findStandardDeviation(arraySet.length - 1).toFixed(10));
+            row.appendChild(head);
+            row = document.createElement("tr");
+            tableFoot.appendChild(row);
+            head = document.createElement("th");
+            head.colSpan = 2;
+            head.textContent = "Population Standard Deviation (σ)";
+            row.appendChild(head);
+            head = document.createElement("th");
+            head.colSpan = 2;
+            head.textContent = parseFloat(findStandardDeviation(arraySet.length).toFixed(10));
+            row.appendChild(head);
+        })();
+    });
+    const resetTable = (() => {
+        while(tableHead.firstChild) {
+            tableHead.removeChild(tableHead.lastChild);
+        }
+        while(tableBody.firstChild) {
+            tableBody.removeChild(tableBody.lastChild);
+        }
+        while(tableFoot.firstChild) {
+            tableFoot.removeChild(tableFoot.lastChild);
+        }
     });
 })();
