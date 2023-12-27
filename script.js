b18f01d307d592294389b0bb601b691be9b244b6
Matt <matthewjosefcruz@gmail.com>
2023-12-27 20:31:13 +0800

    Fix Error Where Empty Element Converts Into 0 On Table

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ const table = (() => {
         convertArray(textInput.value);
         if(!isValid()) return;
         updateText();
-        console.log("It is valid!");//
         generateTable();
     });
 
@@ -25,7 +24,6 @@ const table = (() => {
             arraySet[i] = arraySet[i].trim(); //remove whitespaces before & after element
         }
         if(arraySet[arraySet.length - 1] === "") arraySet.pop(); //remove empty space when "," last
-        console.log(arraySet);//
     });
     const isValid = (() => {
         error.textContent = "";
@@ -33,7 +31,7 @@ const table = (() => {
             error.textContent = "Please add more data :)";
             return false;
         }
-        if(arraySet.some((element) => isNaN(element))) {
+        if(arraySet.some((element) => isNaN(element) || element === "")) {
             error.textContent = "Invalid Input, please try again :o";
             return false;
         }
